d0c1ec6e2b55e1320eec64a44ede429a34ebca62
jhonatan050 <jhonatanchango9@gmail.com>
2026-03-25 21:17:11 -0500

    Clase 12 - Crear archivo.txt de plantilla y proyecto para practicar

---
+++ Java/ejerciciosClase13/Ejercicio1/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args){ //Metodo principal
+        Persona persona1 = new Persona(); //Aqui creamos un objeto de tipo Persona llamado persona1
+
+        persona1.nombre = "Carlos"; //Asignamos un valor al atributo nombre del objeto 
+
+        persona1.edad = 20; //Asignamos un valor al atributo edad del objeto
+
+        System.out.println("Nombre: "+ persona1.nombre); //Mostramos el nombre en consola
+        System.out.println("Edad: "+ persona1.edad); //Mostrar la edad en consola
+    }//Fin del metodo main
+}//Fin de la clase App
---
+++ Java/ejerciciosClase13/Ejercicio1/src/Persona.java
@@ -0,0 +1,5 @@
+public class Persona {
+    String nombre; //Atributo que guardara el nombre de la Persona 
+    int edad; //Atributo que guarda la edad de la Persona 
+}
+
